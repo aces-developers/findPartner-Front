d0f760abfd359c4b02b7e23cc0e5954667b341c9
HidayaSyam <hid.sy1910@gmail.com>
2020-11-04 16:05:27 +0200

    Home page - ProjectStore - UserStore - list Card - Card Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Component/header/header'
 import Jumber from './Component/jumbotron/jumbotron'
 import Ues from './Component/sectionUse/sectionUse'
+import ListProject from './store/projects/listproject/list-projects'
 import { Provider } from "react-redux";
 import store from "./store/";
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Header/>
     <Jumber/>
     <Ues/>
+    <ListProject/>
     </Provider>
     </>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 
 import projects from './projects/project.store';
+import users from './users/users.store'
 
-
-const reducers = combineReducers({projects: projects})
+const reducers = combineReducers({projects: projects,users: users})
 const store = configureStore({ reducer: reducers});
 
 export default store;
