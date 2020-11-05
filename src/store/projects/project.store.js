17cc3fdb79a4188aad9a7f2dc2c2dc205f64d486
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-11-05 12:23:49 +0200

    fix infinit loop"

--- src/store/projects/project.store.js
+++ src/store/projects/project.store.js
@@ -4,44 +4,48 @@ const projects = createSlice({
   name: "projects",
   initialState: {
     projects: [],
+    searchResult: [],
   },
   reducers: {
     setProjects(state, action) {
-      console.log('action =====>', action);
-        state.projects = action.payload ;
+      console.log("action =====>", action);
+      state.projects = action.payload;
+    },
+    setSearchResult(state, action) {
+      console.log("setSearchResults =====>", action);
+      state.searchResult = action.payload;
     },
   },
 });
 
 export const loadProjects = () => async (dispatch, getState) => {
-    axios
-      .get("https://as-findpartner.herokuapp.com/allprojects")
-      .then((res) => {
-        // handle success
-        console.log(' handle success-->',res.data);
-          dispatch(setProjects(res.data));
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
-  };
+  axios
+    .get("https://as-findpartner.herokuapp.com/allprojects")
+    .then((res) => {
+      // handle success
+      console.log(" loadProjects success-->", res.data);
+      dispatch(setProjects(res.data));
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
+};
 
-export const {  setProjects } = projects.actions;
-
-
-  export const handleSearch = (q,search) => async (dispatch, getState) => {
-    axios
-      .get(`https://as-findpartner.herokuapp.com/allprojects?q=${q}&search=${search}`)
-      .then((res) => {
-        // handle success
-        console.log(' handle success-->',res.data);
-          dispatch(setProjects(res.data));
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
-  };
-export const {  setProjects } = projects.actions;
+export const handleSearch = (q, search) => async (dispatch, getState) => {
+  axios
+    .get(
+      `https://as-findpartner.herokuapp.com/allprojects?q=${q}&search=${search}`
+    )
+    .then((res) => {
+      // handle success
+      console.log(" handleSearch success-->", res.data);
+      dispatch(setSearchResult(res.data));
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
+};
+export const { setProjects, setSearchResult } = projects.actions;
 export default projects.reducer;
