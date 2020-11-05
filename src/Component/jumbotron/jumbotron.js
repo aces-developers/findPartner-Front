0d553f4aee9ee03bd88d16db5cc97736a3f6c154
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-11-05 10:03:20 +0200

    ui changes in home page

--- src/Component/jumbotron/jumbotron.js
+++ src/Component/jumbotron/jumbotron.js
@@ -1,22 +1,64 @@
-import React from 'react'
-import { Jumbotron, Button } from 'react-bootstrap'
-
+import React from "react";
+import { Jumbotron, Button, Container } from "react-bootstrap";
 
 export default function Jumber(props) {
-    return (
-        <>
-            <Jumbotron style={{height:"400px",backgroundImage: "url(https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)",backgroundSize: "cover"}}>
-            <h1>Hello, world!</h1>
-                <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-                </p>
+  const styles = {
+    jumbotron: {
+      height: 600,
+      backgroundImage:
+        "linear-gradient( to left, rgba(0,180,219, 0.7), rgba(0,103,139, 0.7)),url('https://images.pexels.com/photos/3182759/pexels-photo-3182759.jpeg?cs=srgb&dl=pexels-fauxels-3182759.jpg&fm=jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "50% 50%",
+      paddingTop: 300,
+    },
+    text: {
+      color: "white",
+      fontSize: 30,
+      fontWeight: 500,
+      marginBottom: 60,
+    },
+    btn: {
+      backgroundColor: "#00B4DB",
+      color: "#fff",
+      border: "none",
+      marginRight: 20,
+      paddingTop: 10,
+      paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontWeight: 500,
+      "&:hover": {
+        backgroundColor: "#fff",
+      },
+    },
 
-            <p>
-                <Button  variant="primary">Find a Partner</Button>{' '}
-                <Button variant="primary">Join a Project</Button>
-            </p>
-        </Jumbotron>
-        </>
-    )
+    btn2: {
+      backgroundColor: "#fff",
+      color: "#0083B0",
+      border: "none",
+      paddingTop: 10,
+      paddingBottom: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontWeight: 500,
+    },
+  };
+
+  return (
+    <>
+      <Jumbotron fluid style={styles.jumbotron}>
+        <Container>
+          <p style={styles.text}>
+            If opportunity doesn't knock we will open a door
+          </p>
+          <p>
+            <Button className="btn" style={styles.btn}>
+              Find a Partner
+            </Button>
+            <Button style={styles.btn2}>Join a Project</Button>
+          </p>
+        </Container>
+      </Jumbotron>
+    </>
+  );
 }
