fbc83c9abc7cf9ddbaf8b18040865088a494326d
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-11-06 20:39:30 +0200

    update readme

--- src/store/users/users.store.js
+++ src/store/users/users.store.js
@@ -5,30 +5,32 @@ const users = createSlice({
   name: "users",
   initialState: {
     users: [],
-    
+    account: {
+      token: null,
+      user: null,
+    },
   },
   reducers: {
     setUsers(state, action) {
-      console.log('action =====>', action);
-        state.users = action.payload ;
+      console.log("action =====>", action);
+      state.users = action.payload;
     },
   },
 });
 
 export const loadUsers = () => async (dispatch, getState) => {
-    axios
-      .get("https://as-findpartner.herokuapp.com/users")
-      .then((res) => {
-        // handle success
-        console.log(' handle success-->',res.data);
-          dispatch(setUsers(res.data));
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
-  };
-  
+  axios
+    .get("https://as-findpartner.herokuapp.com/users")
+    .then((res) => {
+      // handle success
+      console.log(" handle success-->", res.data);
+      dispatch(setUsers(res.data));
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
+};
 
 export const { setUsers } = users.actions;
 
