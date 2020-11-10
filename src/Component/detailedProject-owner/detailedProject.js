7c43616f1190742bf6ef2179a11a878e1d96819a
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-11-10 23:45:48 +0200

    style dtails page

--- src/Component/detailedProject-owner/detailedProject.js
+++ src/Component/detailedProject-owner/detailedProject.js
@@ -1,95 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import { connect, useDispatch } from 'react-redux'
-import { Nav, Form } from "react-bootstrap"
-import { Details } from "./details"
-import { Propsals } from './propsals'
-import {ModalSwitch} from './ModalSwitch'
-import {DeleteModal} from './DeleteModel'
-import {setModal,getproject, setcheck} from "../../store/projects/project.store"
-
-
+import React, { useEffect, useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import { Nav, Form, Container } from "react-bootstrap";
+import { Details } from "./details";
+import { Propsals } from "./propsals";
+import { ModalSwitch } from "./ModalSwitch";
+import { DeleteModal } from "./DeleteModel";
+import {
+  setModal,
+  getproject,
+  setcheck,
+} from "../../store/projects/project.store";
 
 export const DetailedProject = (props) => {
-
-    function Page() {
-        if (view) {
-            return <Details props={props.props}/>
-        }
-        else {
-            return <Propsals />
-        }
+  function Page() {
+    if (view) {
+      return <Details props={props.props} />;
+    } else {
+      return <Propsals />;
     }
-    function Modals() {
-        if(props.Modal){
-            return <ModalSwitch props={props} />
-        }else if (props.props.deleteModal){
-            return <DeleteModal props={props} />
-        }else return ( <></>)
-    }
-  
-    console.log('dm',props.props)
-    // console.log('props.projectData[0].isopen',props.projectData[0].isopen)
+  }
+  function Modals() {
+    if (props.Modal) {
+      return <ModalSwitch props={props} />;
+    } else if (props.props.deleteModal) {
+      return <DeleteModal props={props} />;
+    } else return <></>;
+  }
 
-    const [view, setview] = useState(true)
-    const dispatch = useDispatch()
-    const toggleEnabled = ()=>{ dispatch(setModal(true)) }
-    const openDetails = () => { setview(true) }
-    const openProposals = () => { setview(false) }
-    const  _id  = props.props.match.params._id
-    console.log(_id)
-    // useEffect(() => {
-    //   const load = async () => {
-    //     await dispatch(getproject(_id));
-    //     console.log('props--->props', props.props)
-        
-    // };
-    // load();
+  console.log("dm", props.props);
+  // console.log('props.projectData[0].isopen',props.projectData[0].isopen)
 
-    // }, [dispatch])
+  const [view, setview] = useState(true);
+  const dispatch = useDispatch();
+  const toggleEnabled = () => {
+    dispatch(setModal(true));
+  };
+  const openDetails = () => {
+    setview(true);
+  };
+  const openProposals = () => {
+    setview(false);
+  };
+  const _id = props.props.match.params._id;
+  console.log(_id);
+  // useEffect(() => {
+  //   const load = async () => {
+  //     await dispatch(getproject(_id));
+  //     console.log('props--->props', props.props)
 
-    return (
-        <> 
-         {props.props.projectData &&
+  // };
+  // load();
+
+  // }, [dispatch])
+
+  return (
+    <>
+      <div className="signup-section" style={{ minHeight: "70vh" }}>
+        <Container>
+          {props.props.projectData && (
             <Form>
-                <Form.Check
-                    type="switch"
-                    id="custom-switch"
-                    label="Close project"
-                    //  checked={props.props.check}
-                    onChange={toggleEnabled} 
-                />
+              <Form.Check
+                type="switch"
+                id="custom-switch"
+                label="Close project"
+                //  checked={props.props.check}
+                onChange={toggleEnabled}
+              />
             </Form>
-}
-            <Nav variant="pills" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link onClick={openDetails}>Details</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link onClick={openProposals}>Propasls</Nav.Link>
-                </Nav.Item>
-           
-            </Nav>
-
-            <Page />
-
-            <Modals />
-        </>
-    )
-}
-
-
+          )}
+          <Nav variant="pills" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link onClick={openDetails}>Details</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={openProposals}>Propasls</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Page />
+          <Modals />
+        </Container>
+      </div>
+    </>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    details: state.projects.projectDetails,
-    Modal:state.projects.Modal,
-    projectData: state.projects.projectData,
-    deleteModal:state.projects.deleteModal,
-    account:state.users.account,
-    check:state.projects.check
-    
+  details: state.projects.projectDetails,
+  Modal: state.projects.Modal,
+  projectData: state.projects.projectData,
+  deleteModal: state.projects.deleteModal,
+  account: state.users.account,
+  check: state.projects.check,
+});
 
-})
-
-
-
-export default connect(mapStateToProps)(DetailedProject)
+export default connect(mapStateToProps)(DetailedProject);
--- src/Component/projects/projectDetails.js
+++ src/Component/projects/projectDetails.js
@@ -1,95 +1,135 @@
 import React, { useEffect } from "react";
-import { Alert, Card, Form, Button } from "react-bootstrap";
+import {
+  Alert,
+  Card,
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 import { connect, useDispatch } from "react-redux";
 import { getproject, apply } from "../../store/projects/project.store";
 import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
+
 const Schema = Yup.object().shape({
-    proposal: Yup.string()
-        .min(15, "Too Short!")
-        .required("proposal is required"),
+  proposal: Yup.string().min(15, "Too Short!").required("proposal is required"),
 });
 function Details(props) {
-    const dispatch = useDispatch();
-    const { _id } = props.props.match.params
-    console.log(_id)
-    // useEffect(() => {
-    //     const load = async () => {
-    //         await dispatch(getproject(_id));
-    //         console.log('props--->props', props)
-    //     };
-    //     load();
-    // }, []);
+  const dispatch = useDispatch();
+  const { _id } = props.props.match.params;
 
-    //console.log('props ---> ', )
-    return (
-        <>
-
-            {props.projectData &&
-                <div>
-                    <Card style={{ width: '25rem' }}>
-                        <Card.Body>
-                            <Card.Title>{props.projectData[0].title}</Card.Title>
-                            <Card.Text>{props.projectData[0]._ownerName} </Card.Text>
-                            <Card.Text>{props.projectData[0].category} </Card.Text>
-
-                            <Card.Text>
-                                {props.projectData[0].description}
-                            </Card.Text>
-
-                        </Card.Body>
-                    </Card>
-                    <Formik
-                        validationSchema={Schema}
-                        onSubmit={async (values, { setSubmitting }) => {
-                            await dispatch(apply(props.location.state._id, values))
-                            //console.log('  isValid: state.users.isValid,', props.isValid);
-                            setTimeout(() => {
-
-                                props.history.push("/")
-
-                            }, 4000);
-
-                        }}
-                        initialValues={{
-                            proposal: '',
-
-                        }}
+  return (
+    <>
+      {props.projectData && (
+        <div className="signup-section" style={{ minHeight: "70vh" }}>
+          <Container>
+            <Row className="justify-content-center">
+              <Col md={12}>
+                <h4 className="mb-4" style={{ color: "#0083b0" }}>
+                  Project details
+                </h4>
+                <Card className="w-100 shadow-sm border-0">
+                  <Card.Body>
+                    <Card.Title
+                      style={{ textTransform: "capitalize", color: "#333" }}
                     >
-                        {({
-                            handleSubmit,
-                            handleChange,
-                            handleBlur,
-                            setFieldValue,
-                            values,
-                            touched,
-                            isValid,
-                            errors,
-                        }) => (
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                                        <Form.Label>Propsal</Form.Label>
-                                        <Form.Control name="proposal" onChange={handleChange} isInvalid={!!errors.proposal} value={values.proposal} rows={3} />
-                                        <Form.Control.Feedback type="invalid" >
-                                            {errors.proposal}
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-                                    <Button type="submit">Apply</Button>
-                                </Form>
-                            )}
-                    </Formik>
-                </div>
-            }
-        </>
-    )
+                      {props.projectData[0].title}
+                    </Card.Title>
+                    <Card.Text style={{ fontSize: 12, fontWeight: 300 }}>
+                      <FontAwesomeIcon
+                        icon={faUser}
+                        className="mr-2"
+                        style={{ color: "#00b4db" }}
+                      />
+                      {props.projectData[0]._ownerName}
+                    </Card.Text>
+                    <hr></hr>
+                    <Card.Text>{props.projectData[0].description}</Card.Text>
+                    <Card.Text
+                      className="mt-4"
+                      style={{ textTransform: "capitalize" }}
+                    >
+                      <FontAwesomeIcon
+                        icon={faTag}
+                        className="mr-2"
+                        style={{ color: "#f77d0e" }}
+                      />
+                      {props.projectData[0].category}{" "}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+
+            <Formik
+              validationSchema={Schema}
+              onSubmit={async (values, { setSubmitting }) => {
+                await dispatch(apply(props.location.state._id, values));
+                //console.log('  isValid: state.users.isValid,', props.isValid);
+                setTimeout(() => {
+                  props.history.push("/");
+                }, 4000);
+              }}
+              initialValues={{
+                proposal: "",
+              }}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                setFieldValue,
+                values,
+                touched,
+                isValid,
+                errors,
+              }) => (
+                <Form className="mt-4" onSubmit={handleSubmit}>
+                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label style={{ color: "#0083b0", fontWeight: 600 }}>
+                      Propsal
+                    </Form.Label>
+                    <Form.Control
+                      name="proposal"
+                      onChange={handleChange}
+                      isInvalid={!!errors.proposal}
+                      value={values.proposal}
+                      as="textarea"
+                      rows={7}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.proposal}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button
+                    type="submit"
+                    className="con-linkedin d-block mt-4 ml-auto"
+                    style={{
+                      width: "fit-content",
+                      fontWeight: 600,
+                      paddingLeft: 30,
+                      paddingRight: 30,
+                    }}
+                  >
+                    Apply
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Container>
+        </div>
+      )}
+    </>
+  );
 }
 
 const mapStateToProps = (state) => ({
-    projectData: state.projects.projectData,
-
+  projectData: state.projects.projectData,
 });
 
-
 export default connect(mapStateToProps)(Details);
-
-
