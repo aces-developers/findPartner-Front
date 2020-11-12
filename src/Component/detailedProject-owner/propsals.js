a92ba38b207ebd5df9b74826a4a4a4c78f942dd1
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-11-12 08:31:27 +0200

    adding some style

--- src/Component/detailedProject-owner/propsals.js
+++ src/Component/detailedProject-owner/propsals.js
@@ -4,45 +4,73 @@ import { connect, useDispatch } from "react-redux";
 //import Card from "../projects/card/p-Card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faTag } from "@fortawesome/free-solid-svg-icons";
-
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export const Propsals = (props) => {
   console.log("2234567890", props.props);
-   const variant = "dark"
+  const variant = "dark";
   return (
     <>
-       {props.props.proposalData && props.props.proposalData.length === 0 && <Alert className="mt-4"  variant={variant}> There is no propsals yet ! Stay around though ...</Alert> }
+      {props.props.proposalData && props.props.proposalData.length === 0 && (
+        <Alert className="mt-4" variant={variant}>
+          There is no propsals yet ! Stay around though ...
+        </Alert>
+      )}
       {props.props.proposalData && (
         <div>
-          
-          {props.props.proposalData.map((item, index) => {
-            return (
-              <div key={index}>
-                <Container>
-                  <Row>
-                    <Col>
-                      <Card className="card-style">
-                        <Card.Body className="body-style">
-                          <Card.Text style={{ fontSize: 20, fontWeight: 300 }}>
-                            <FontAwesomeIcon
-                              icon={faUser}
-                              className="mr-2"
-                              style={{ fontSize: 20 }}
-                              style={{ color: "#00b4db" }}
-                            />
-                            {item.username}
-                          </Card.Text>
-                          <Card.Text>{item.email}</Card.Text>
-                          <Card.Text>{item.proposal}</Card.Text>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  </Row>
-                </Container>
-              </div>
-            );
-          })}
+          <Container>
+            <Row>
+              <Col>
+                <Card
+                  className="border-0 m-0 w-100 p-2"
+                  hidden={props.props.proposalData.length === 0}
+                >
+                  {props.props.proposalData.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <Card
+                          className="  p-4 w-75 m-3"
+                          style={{ backgroundColor: "#F7F7FE" }}
+                        >
+                          <Card.Body>
+                            <Card.Text
+                              style={{ fontSize: 20, fontWeight: 500 }}
+                            >
+                              <FontAwesomeIcon
+                                icon={faUser}
+                                className="mr-2"
+                                style={{ fontSize: 20 }}
+                                style={{ color: "#00b4db" }}
+                              />
+                              {item.username}
+                            </Card.Text>
+                            <Card.Text
+                              style={{
+                                backgroundColor: "#fff",
+                                padding: 20,
+                                borderRadius: 5,
+                              }}
+                            >
+                              {item.proposal}
+                            </Card.Text>
+                            <Card.Text>
+                              <FontAwesomeIcon
+                                icon={faEnvelope}
+                                className="mr-2"
+                                style={{ fontSize: 20 }}
+                                style={{ color: "#F17F3B" }}
+                              />
+                              {item.email}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </div>
+                    );
+                  })}
+                </Card>
+              </Col>
+            </Row>
+          </Container>
         </div>
       )}
     </>
--- src/Component/myProjects/myProjects.js
+++ src/Component/myProjects/myProjects.js
@@ -3,6 +3,8 @@ import { Button, Table, ButtonGroup } from "react-bootstrap";
 import { connect, useDispatch } from "react-redux";
 import { Formik, Field, ErrorMessage } from "formik";
 import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
+
 import {
   getAllApplied,
   getAllUserProjects,
@@ -19,13 +21,13 @@ function MyProjects(props) {
     "Location",
     "Status",
   ];
-  const [flag, setflag] = useState(props.userPublishedProjects)
+  const [flag, setflag] = useState(props.userPublishedProjects);
   const dispatch = useDispatch();
   useEffect(() => {
-    props.userPublishedProjects?setflag(props.userPublishedProjects): setflag(localStorage.getItem(JSON.parse('MINE')))
-  }, [props.userPublishedProjects])
-
-  
+    props.userPublishedProjects
+      ? setflag(props.userPublishedProjects)
+      : setflag(localStorage.getItem(JSON.parse("MINE")));
+  }, [props.userPublishedProjects]);
 
   useEffect(() => {
     // const loadApplayed = async () => {
@@ -34,67 +36,73 @@ function MyProjects(props) {
     // loadApplayed();
 
     const loadPublished = async () => {
-      await dispatch(getAllUserProjects())
-       
-     
+      await dispatch(getAllUserProjects());
     };
     loadPublished();
   }, []);
 
-  const changeFlag = (data) => { setflag(data) }
-    
-
-
+  const changeFlag = (data) => {
+    setflag(data);
+  };
 
   return (
     <>
-     
-      <ButtonGroup size="lg" className="mb-2 switchGroupBtns">
-        <Button
-          onClick={() => changeFlag(props.userPublishedProjects)}
-          className="btn btn-primary btn-lg"
-        >
-          Owner
-        </Button>
-        <Button
-        onClick={() => changeFlag(props.AllAppliedID)}
-          className="btn btn-secondary btn-lg"
-        >
-          Partner
-        </Button>
-      </ButtonGroup>
+      <div className="signup-section">
+        <ButtonGroup size="lg" className="mb-2 switchGroupBtns">
+          <Button
+            onClick={() => changeFlag(props.userPublishedProjects)}
+            className="btn btn-primary btn"
+          >
+            Owner
+          </Button>
+          <Button
+            onClick={() => changeFlag(props.AllAppliedID)}
+            className="btn btn-secondary btn"
+          >
+            Partner
+          </Button>
+        </ButtonGroup>
 
-      <Table
-        responsive
-        hover
-        className="dashboardTable shadow p-3 mb-5 bg-white rounded w-75 p-3 mx-auto "
-      >
-        <thead>
-          <tr>
-            <th>#</th>
-            {tableHeadings.map((_, index) => (
-              <th key={index * 100}>{_}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {  flag.map((project, index) => (
+        <Table
+          responsive
+          hover
+          className="dashboardTable shadow p-3 mb-5 bg-white rounded w-75 p-3 mx-auto "
+        >
+          <thead>
             <tr>
-              <td>{index }</td>
-
-              <td key={index}>{project.title}</td>
-              <td key={index}>{project.budget}</td>
-              <td key={index}>{project.category.toUpperCase()}</td>
-              <td key={index}>{`${project.lacation}`}</td>
-              <td key={index} className={`${project.isopen}`}>
-              {project.isopen?'open':'closed'}
-              </td>
+              <th>#</th>
+              {tableHeadings.map((_, index) => (
+                <th key={index * 100}>{_}</th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {flag.map((project, index) => (
+              <tr>
+                <td>{index}</td>
 
-  
+                <td key={index}>
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={{
+                      pathname: `/detalis/${project._id}`,
+                      state: { _id: project._id },
+                    }}
+                  >
+                    {project.title}
+                  </Link>
+                </td>
+                <td key={index}>{project.budget}</td>
+                <td key={index}>{project.category.toUpperCase()}</td>
+                <td key={index}>{`${project.lacation}`}</td>
+                <td key={index} className={`${project.isopen}`}>
+                  {project.isopen ? "open" : "closed"}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </>
   );
 }
@@ -102,6 +110,6 @@ function MyProjects(props) {
 const mapStateToProps = (state) => ({
   AllAppliedID: state.users.AllAppliedID,
   userPublishedProjects: state.users.userPublishedProjects,
-  account:state.users.account
+  account: state.users.account,
 });
 export default connect(mapStateToProps)(MyProjects);
